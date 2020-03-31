b38d1e04efc62cb675bb3e03adc2d309557806ab
mikhail-j <mikhail.jin@gmail.com>
2020-03-31 14:51:20 -0400

    Removed local variable DataType declaration when using with 'let' keyword to avoid crashing.

--- test/cuda_complex_tests.jl
+++ test/cuda_complex_tests.jl
@@ -20,62 +20,62 @@
 using NVIDIALibraries.ComplexTypes
 
 # cuConjf()
-let x::cuComplex = cuComplex(111.1f0, 111.1f0)
+let x = cuComplex(111.1f0, 111.1f0)
     @test (pop!(collect(reinterpret(Complex{Float32}, [cuConjf(x)]))) == conj(Complex{Float32}(111.1f0, 111.1f0)))
 end
 
 # cuConj()
-let x::cuDoubleComplex = cuDoubleComplex(111.1, 111.1)
+let x = cuDoubleComplex(111.1, 111.1)
     @test (pop!(collect(reinterpret(Complex{Float64}, [cuConj(x)]))) == conj(Complex{Float64}(111.1, 111.1)))
 end
 
 # cuCaddf()
-let x::cuComplex = cuComplex(111.1f0, 111.1f0), y::cuComplex = cuComplex(111.1f0, 111.1f0)
+let x = cuComplex(111.1f0, 111.1f0), y = cuComplex(111.1f0, 111.1f0)
     @test (pop!(collect(reinterpret(Complex{Float32}, [cuCaddf(x, y)]))) == (Complex{Float32}(111.1f0, 111.1f0) + Complex{Float32}(111.1f0, 111.1f0)))
 end
 
 # cuCadd()
-let x::cuDoubleComplex = cuDoubleComplex(111.1, 111.1), y::cuDoubleComplex = cuDoubleComplex(111.1, 111.1)
+let x = cuDoubleComplex(111.1, 111.1), y = cuDoubleComplex(111.1, 111.1)
     @test (pop!(collect(reinterpret(Complex{Float64}, [cuCadd(x, y)]))) == (Complex{Float64}(111.1, 111.1) + Complex{Float64}(111.1, 111.1)))
 end
 
 # cuCsubf()
-let x::cuComplex = cuComplex(111.1f0, 111.1f0), y::cuComplex = cuComplex(11.1f0, 11.1f0)
+let x = cuComplex(111.1f0, 111.1f0), y = cuComplex(11.1f0, 11.1f0)
     @test (pop!(collect(reinterpret(Complex{Float32}, [cuCsubf(x, y)]))) == (Complex{Float32}(111.1f0, 111.1f0) - Complex{Float32}(11.1f0, 11.1f0)))
 end
 
 # cuCsub()
-let x::cuDoubleComplex = cuDoubleComplex(111.1, 111.1), y::cuDoubleComplex = cuDoubleComplex(11.1, 11.1)
+let x = cuDoubleComplex(111.1, 111.1), y = cuDoubleComplex(11.1, 11.1)
     @test (pop!(collect(reinterpret(Complex{Float64}, [cuCsub(x, y)]))) == (Complex{Float64}(111.1, 111.1) - Complex{Float64}(11.1, 11.1)))
 end
 
 # cuCmulf()
-let x::cuComplex = cuComplex(111.1f0, 111.1f0), y::cuComplex = cuComplex(111.1f0, 111.1f0)
+let x = cuComplex(111.1f0, 111.1f0), y = cuComplex(111.1f0, 111.1f0)
     @test (pop!(collect(reinterpret(Complex{Float32}, [cuCmulf(x, y)]))) == (Complex{Float32}(111.1f0, 111.1f0) * Complex{Float32}(111.1f0, 111.1f0)))
 end
 
 # cuCmul()
-let x::cuDoubleComplex = cuDoubleComplex(111.1, 111.1), y::cuDoubleComplex = cuDoubleComplex(111.1, 111.1)
+let x = cuDoubleComplex(111.1, 111.1), y = cuDoubleComplex(111.1, 111.1)
     @test (pop!(collect(reinterpret(Complex{Float64}, [cuCmul(x, y)]))) == (Complex{Float64}(111.1, 111.1) * Complex{Float64}(111.1, 111.1)))
 end
 
 # cuCdivf()
-let x::cuComplex = cuComplex(111.1f0, 111.1f0), y::cuComplex = cuComplex(11.1f0, 11.1f0)
+let x = cuComplex(111.1f0, 111.1f0), y = cuComplex(11.1f0, 11.1f0)
     @test (pop!(collect(reinterpret(Complex{Float32}, [cuCdivf(x, y)]))) == (Complex{Float32}(111.1f0, 111.1f0) / Complex{Float32}(11.1f0, 11.1f0)))
 end
 
 # cuCdiv()
-let x::cuDoubleComplex = cuDoubleComplex(111.1, 111.1), y::cuDoubleComplex = cuDoubleComplex(11.1, 11.1)
+let x = cuDoubleComplex(111.1, 111.1), y = cuDoubleComplex(11.1, 11.1)
     @test (pop!(collect(reinterpret(Complex{Float64}, [cuCdiv(x, y)]))) == (Complex{Float64}(111.1, 111.1) / Complex{Float64}(11.1, 11.1)))
 end
 
 # cuCabsf()
-let x::cuComplex = cuComplex(111.1f0, 111.1f0)
+let x = cuComplex(111.1f0, 111.1f0)
     @test (cuCabsf(x) == abs(Complex{Float32}(111.1f0, 111.1f0)))
 end
 
 # cuCabs()
-let x::cuDoubleComplex = cuDoubleComplex(111.1, 111.1)
+let x = cuDoubleComplex(111.1, 111.1)
     @test (cuCabs(x) == abs(Complex{Float64}(111.1, 111.1)))
 end
 
